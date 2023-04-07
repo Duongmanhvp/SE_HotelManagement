a1c3f8052ee3af2e55857d5e2b5116a30137faa9
Soerika <alephnosin@gmail.com>
2023-04-07 12:22:42 +0700

    minor fix

--- api/models.py
+++ api/models.py
@@ -5,7 +5,8 @@ class User(models.Model):
     id_number = models.IntegerField(null=False, default=0, unique=True)
     name = models.CharField(max_length=200, null=False)
     email = models.CharField(max_length=200, null=True)
-    isAdmin = models.BooleanField(default=False)
+    password = models.CharField(max_length=40, null=False)
+    is_admin = models.BooleanField(default=False)
 
     def __str__(self) -> str:
         return self.name
@@ -13,7 +14,7 @@ class User(models.Model):
 class Hotel(models.Model):
     name = models.CharField(max_length=200)
     address = models.CharField(max_length=200, null=True)
-    starRate = models.IntegerField(default=5)
+    rating = models.IntegerField(default=5)
 
     def __str__(self) -> str:
         return self.name
@@ -23,17 +24,12 @@ class Room(models.Model):
     type = models.CharField(max_length=50, default='standard')
     price = models.FloatField(default=0.00)
     description = models.CharField(max_length=200, default='')
-    available = models.BooleanField(default=True)
+    is_available = models.BooleanField(default=True)
 
 class Service(models.Model):
     name = models.CharField(max_length=50)
     price = models.FloatField(default=0.00)
     last_updated = models.DateTimeField(auto_now=True)
-    
-class ServiceList(models.Model):
-    service_id = models.IntegerField(null=False)
-    service_used = models.ForeignKey(Service, on_delete=models.CASCADE)
-    amount = models.IntegerField(default=0)
 
 class BookingDetail(models.Model):
     hotel_id = models.ForeignKey(Hotel, on_delete=models.CASCADE)
@@ -47,12 +43,10 @@ class Billing(models.Model):
     booking_detail_id = models.ForeignKey(BookingDetail, on_delete=models.CASCADE)
     customer = models.ForeignKey(User, on_delete=models.CASCADE)
     total = models.FloatField(default=0.00)
-    payment_date = models.DateTimeField()
+    pay_date = models.DateTimeField()
     payment_type = models.CharField(max_length=50)
     
 
 class Comment(models.Model):
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.CharField(max_length=256, default='')
-    rating = models.IntegerField(default=5)
-
--- api/urls.py
+++ api/urls.py
@@ -3,7 +3,16 @@ from . import views
 
 urlpatterns = [
     path('', views.apiOverviews, name='overviews'),
+
+    # user controller api
     path('users/', views.getAllUsers, name='users'),
     path('users/<str:pk>/', views.getUser, name='query_user'),
     path('register/', views.register, name='register'),
+
+    # booking interface controller api
+    # path('hotel/',)
+
+    # billing controller api
+
+    # finnancial admin controller api
 ]
